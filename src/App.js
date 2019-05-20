c9a291b7069bf3214cbd065e05c5c0574a4fdd36
gainaa <kajka.c@gmail.com>
2019-05-20 18:01:01 +0200

    setState added, everything is ready to create add form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css';
+import { RaceOperator } from 'rxjs/internal/observable/race';
 
-const App = () => (
-    <div>
-        <ListWrapper />
-    </div>
-);
+const myData = [
+    {
+        image: 'http://www.janusznosacz.pl/sk/obrazki/2018/02/ba51dad217cf509bd47532848ea0665a.jpg',
+        name: 'Jadłonomia',
+        description: 'Podróże i najpyszniejsze w sieci wegańskie jedzenie.',
+        instagramLink: 'https://www.instagram.com/jadlonomia/',
+    },
+    {
+        image: 'http://www.janusznosacz.pl/sk/obrazki/2018/02/ba51dad217cf509bd47532848ea0665a.jpg',
+        name: 'Weganon',
+        description: 'Wegańskie gotowanie po męsku. Dużo przepisów na pyszne i zdrowe słodkości.',
+        instagramLink: 'https://www.instagram.com/weganon/',
+    },
+    {
+        image: 'http://www.janusznosacz.pl/sk/obrazki/2018/02/ba51dad217cf509bd47532848ea0665a.jpg',
+        name: 'Minimalist Baker',
+        description: 'Wegański food porn. Mało składników, szybkie, piękne i pyszne dania.',
+        instagramLink: 'https://www.instagram.com/minimalistbaker/',
+    },
+    {
+        image: 'http://www.janusznosacz.pl/sk/obrazki/2018/02/ba51dad217cf509bd47532848ea0665a.jpg',
+        name: 'Wegan Nerd',
+        description: 'Dużo inspiracji na wegańskie przepisy. Fajne pomysły na dania kuchni azjatyckiej.',
+        instagramLink: 'https://www.instagram.com/wegannerdblog/',
+    },
+]
+
+class App extends React.Component {
+
+    state = {
+        items: [...myData],
+    }
+
+    render() {
+        return (
+            <div>
+                <ListWrapper
+                    items={this.state.items}
+                />
+            </div>
+        )
+    }
+
+};
 
 export default App;
--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
-import { instagramAccounts } from '../../data/instagramAccounts';
+import './ListWrapper.css';
 
-const ListWrapper = () => (
+const ListWrapper = (props) => (
     <ul className="listWrapper__wrapper">
-        {/* renderuje wszystko przy uzyciu propsow, rozwiazanie to przy kazdym itemie powtarza kod w zwiazku z czym zostalo zamienione na mapowanie 
-        <ListItem
-            name={instagramAccounts[0].name}
-            description={instagramAccounts[0].description}
-            image={instagramAccounts[0].image}
-            link={instagramAccounts[0].instagramLink}
-        /> */}
-
-        {instagramAccounts.map(item => (
+        {props.items.map(item => (
             <ListItem
                 key={item.name}
                 {...item}
