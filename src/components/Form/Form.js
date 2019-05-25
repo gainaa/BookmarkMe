7322f8be885edfcea80e45057caff91a42389797
gainaa <kajka.c@gmail.com>
2019-05-25 19:14:50 +0200

    added radio buttons to form, since now form is dynamic

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,35 +4,83 @@ import Input from '../Input/Input';
 import Button from '../Button/Button';
 import Title from '../Title/Title';
 
-const Form = ({ submitFn }) => (
-    <div className={styles.wrapper}>
-        <Title>Add new instagram account</Title>
-        <form
-            autoComplete="off"
-            className={styles.form}
-            onSubmit={submitFn}
-        >
-            <Input
-                name="name"
-                label="Name"
-                maxLength={30}
-            />
-            <Input
-                name="link"
-                label="Instagram link"
-            />
-            <Input
-                name="image"
-                label="Image"
-            />
-            <Input
-                tag="textarea"
-                name="description"
-                label="Description"
-            />
-            <Button>add new item</Button>
-        </form>
-    </div>
-);
+const types = {
+    instagram: 'instagram',
+    article: 'article',
+    note: 'note',
+}
+
+const descriptions = {
+    instagram: 'favorite Instagram account',
+    article: 'Article good to remember',
+    note: 'personal Note',
+}
+
+class Form extends React.Component {
+
+    state = {
+        activeOption: types.instagram,
+    }
+
+    handleRadioButtonChange = (type) => {
+        this.setState({
+            activeOption: type,
+        })
+    }
+
+    render() {
+        return (
+            <div className={styles.wrapper}>
+                <Title>Add new {descriptions[this.state.activeOption]}</Title>
+                <form
+                    autoComplete="off"
+                    className={styles.form}
+                    onSubmit={this.props.submitFn}
+                >
+                    <input
+                        id={types.instagram}
+                        type="radio"
+                        checked={this.state.activeOption === types.instagram}
+                        onChange={() => this.handleRadioButtonChange(types.instagram)}
+                    />
+                    <label for={types.instagram}>Instagram</label>
+                    <input
+                        id={types.article}
+                        type="radio"
+                        checked={this.state.activeOption === types.article}
+                        onChange={() => this.handleRadioButtonChange(types.article)}
+                    />
+                    <label for={types.article}>Article</label>
+                    <input
+                        id={types.note}
+                        type="radio"
+                        checked={this.state.activeOption === types.note}
+                        onChange={() => this.handleRadioButtonChange(types.note)}
+                    />
+                    <label for={types.note}>Note</label>
+                    <Input
+                        name="name"
+                        label="Name"
+                        maxLength={30}
+                    />
+                    <Input
+                        name="link"
+                        label="Instagram link"
+                    />
+                    <Input
+                        name="image"
+                        label="Image"
+                    />
+                    <Input
+                        tag="textarea"
+                        name="description"
+                        label="Description"
+                    />
+                    <Button>add new item</Button>
+                </form>
+            </div>
+        )
+    }
+}
 
 export default Form;
