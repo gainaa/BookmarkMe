c78bdcb36f0b89bb05760596b7cd14c0d4450c16
gainaa <kajka.c@gmail.com>
2019-05-26 10:30:51 +0200

    finished modal, styled radio buttons and added conditional rendering for form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,6 +3,7 @@ import styles from './Form.module.scss';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
 import Title from '../Title/Title';
+import Radio from "./FormRadio";
 
 const types = {
     instagram: 'instagram',
@@ -22,55 +23,64 @@ class Form extends React.Component {
         activeOption: types.instagram,
     }
 
-    handleRadioButtonChange = (type) => {
+    handleRadioButtonChange = type => {
         this.setState({
             activeOption: type,
-        })
-    }
+        });
+    };
 
     render() {
+        const { activeOption } = this.state;
+
         return (
             <div className={styles.wrapper}>
-                <Title>Add new {descriptions[this.state.activeOption]}</Title>
+                <Title>Add new {descriptions[activeOption]}</Title>
                 <form
                     autoComplete="off"
                     className={styles.form}
                     onSubmit={this.props.submitFn}
                 >
-                    <input
-                        id={types.instagram}
-                        type="radio"
-                        checked={this.state.activeOption === types.instagram}
-                        onChange={() => this.handleRadioButtonChange(types.instagram)}
-                    />
-                    <label for={types.instagram}>Instagram</label>
-                    <input
-                        id={types.article}
-                        type="radio"
-                        checked={this.state.activeOption === types.article}
-                        onChange={() => this.handleRadioButtonChange(types.article)}
-                    />
-                    <label for={types.article}>Article</label>
-                    <input
-                        id={types.note}
-                        type="radio"
-                        checked={this.state.activeOption === types.note}
-                        onChange={() => this.handleRadioButtonChange(types.note)}
-                    />
-                    <label for={types.note}>Note</label>
+                    <div className={styles.formOptions}>
+                        <Radio
+                            id={types.instagram}
+                            type="radio"
+                            checked={activeOption === types.instagram}
+                            changeFn={() => this.handleRadioButtonChange(types.instagram)}
+                        >
+                            Instagram
+                        </Radio>
+                        <Radio
+                            id={types.article}
+                            type="radio"
+                            checked={this.state.activeOption === types.article}
+                            changeFn={() => this.handleRadioButtonChange(types.article)}
+                        >
+                            Article
+                        </Radio>
+                        <Radio
+                            id={types.note}
+                            type="radio"
+                            checked={activeOption === types.note}
+                            changeFn={() => this.handleRadioButtonChange(types.note)}
+                        >
+                            Note
+                        </Radio>
+                    </div>
                     <Input
                         name="name"
-                        label="Name"
+                        label={activeOption === types.instagram ? "Instagram Name" : "Title"}
                         maxLength={30}
                     />
-                    <Input
-                        name="link"
-                        label="Instagram link"
-                    />
-                    <Input
-                        name="image"
-                        label="Image"
-                    />
+                    {activeOption !== types.note ? (
+                        <Input
+                            name="link"
+                            label={activeOption === types.instagram ? "Instagram Link" : "Link"}
+                        />
+                    ) : null}
+
+                    {activeOption === types.instagram ? (
+                        <Input name="image" label="Image" />
+                    ) : null}
                     <Input
                         tag="textarea"
                         name="description"
@@ -79,7 +89,7 @@ class Form extends React.Component {
                     <Button>add new item</Button>
                 </form>
             </div>
-        )
+        );
     }
 }
 
